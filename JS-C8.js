bca164b7764b3960b161dd960a11850bc9ddb3f8
Andres Elias <andreselias58690@gmail.com>
2026-02-16 13:09:59 -0500

    coding excercises

---
+++ JS-C8.js
@@ -0,0 +1,10 @@
+const colors = ["red", "green", "blue", "yellow", "orange"];
+
+for(const color of colors) {
+    console.log(color);
+}
+
+// Using for in looop 
+//  loop is best used when you need to loop over
+//  the properties of an object. This loop will iterate over all enumerable properties of an object, including inherited properties and non-numeric properties.
+//Using for in loop is better when you need to loop over
